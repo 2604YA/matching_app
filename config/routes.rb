1bc4b71379ca8a921c3564853eea5c2dc939dd86
Yu Abe <y0u4a2b6e0@gmail.com>
2019-08-06 17:41:03 +0900

    Finish home pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'home_pages#home'
+  get 'home_pages/home'
+  get 'home_pages/help'
+  get 'home_pages/about'
+  get 'home_pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
