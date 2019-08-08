8768903a4d0f837fa7489707347b6935b136c8ce
Yu Abe <y0u4a2b6e0@gmail.com>
2019-08-08 12:50:36 +0900

    ５章修了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'home_pages#home'
-  get 'home_pages/home'
-  get 'home_pages/help'
-  get 'home_pages/about'
-  get 'home_pages/contact'
+  get 'help', to:'home_pages#help'
+  get '/about', to:'home_pages#about'
+  get '/contact', to:'home_pages#contact'
+  get '/signup', to: 'users#new'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
