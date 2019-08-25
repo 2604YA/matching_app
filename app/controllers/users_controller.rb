163b166774dbbf57ae44a5ca1eac7daaeba203a3
Yu Abe <y0u4a2b6e0@gmail.com>
2019-08-26 02:26:05 +0900

    bundler 1.17.3にダウングレードした、サーバーが動くようになった

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   #:index
-  before_action :logged_in_user, only:[:edit, :update, :destroy,
+  before_action :logged_in_user, only:[:edit, :show, :update, :destroy,
   :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
